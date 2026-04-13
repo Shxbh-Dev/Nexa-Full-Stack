973561ec96e02773945aee24f61519006442eaaf
Shubham Singh <spixomxd@kali.kalLinux>
2026-04-13 16:18:47 +0530

    Axios fix 2

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,8 +1,17 @@
-import axios from 'axios'; // <--- THIS MUST BE HERE
+import axios from 'axios'; // <--- THIS LINE IS MANDATORY
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://nexa-full-stack-1.onrender.com',
   withCredentials: true,
 });
+
+// This ensures that even if you use this instance, cookies are forced
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.withCredentials = true;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
